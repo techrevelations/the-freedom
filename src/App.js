b34bfecfbcf040480168203977834946d0a1009c
Team60-Launchpad <77451696+sixoh@users.noreply.github.com>
2021-10-09 21:43:45 -0300

    express server

--- src/App.js
+++ src/App.js
@@ -45,5 +45,16 @@ const App = () => {
       )} />
   );
 }
+const express = require("express")
+const app = express()
+
+app.use(express.static("public"))
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
+
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
 
 export default App;
