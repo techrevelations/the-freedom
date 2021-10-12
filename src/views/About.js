4c24c85a6434afa53f94c9a16fda9fe3208e6c21
techrevelations <cwsmith12@gmail.com>
2021-10-12 11:50:17 -0300

    revert changes

---
+++ src/views/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import sections
+// import Hero from '../components/sections/Hero';
+// import FeaturesTiles from '../components/sections/FeaturesTiles';
+import Contact from '../components/sections/Contact';
+// import Testimonial from '../components/sections/Testimonial';
+import Cta from '../components/sections/Cta';
+
+const Home = () => {
+
+  return (
+    <>
+      {/* <Hero className="illustration-section-01" /> */}
+      {/* <FeaturesTiles /> */}
+      <Contact invertMobile topDivider imageFill className="illustration-section-02" />
+      {/* <Testimonial topDivider /> */}
+      <Cta split />
+    </>
+  );
+}
+
+export default Home;
