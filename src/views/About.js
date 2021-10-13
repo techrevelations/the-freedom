ca896a43ec80ae7ad131297e03605073d7b73466
techrevelations <cwsmith12@gmail.com>
2021-10-13 11:54:21 -0300

    link page

--- src/views/About.js
+++ src/views/About.js
@@ -2,21 +2,21 @@ import React from 'react';
 // import sections
 // import Hero from '../components/sections/Hero';
 // import FeaturesTiles from '../components/sections/FeaturesTiles';
-import Contact from '../components/sections/Contact';
+// import Contact from '../components/sections/Contact';
 // import Testimonial from '../components/sections/Testimonial';
 import Cta from '../components/sections/Cta';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const About = () => {
 
   return (
-    <Link to="/about">
+    <>
       {/* <Hero className="illustration-section-01" /> */}
       {/* <FeaturesTiles /> */}
-      <Contact invertMobile topDivider imageFill className="illustration-section-02" />
+      {/* <Contact invertMobile topDivider imageFill className="illustration-section-02" /> */}
       {/* <Testimonial topDivider /> */}
       <Cta split />
-    </Link>
+    </>
   );
 }
 
