0cf9b521d8638fe6f711c358022be33c5155505f
techrevelations <cwsmith12@gmail.com>
2021-10-13 11:03:42 -0300

    link page

--- src/views/About.js
+++ src/views/About.js
@@ -5,17 +5,18 @@ import React from 'react';
 import Contact from '../components/sections/Contact';
 // import Testimonial from '../components/sections/Testimonial';
 import Cta from '../components/sections/Cta';
+import { Link } from "react-router-dom";
 
 const About = () => {
 
   return (
-    <>
+    <Link to="/about">
       {/* <Hero className="illustration-section-01" /> */}
       {/* <FeaturesTiles /> */}
       <Contact invertMobile topDivider imageFill className="illustration-section-02" />
       {/* <Testimonial topDivider /> */}
       <Cta split />
-    </>
+    </Link>
   );
 }
 
